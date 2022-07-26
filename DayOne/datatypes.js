4d3756647d1b1689c695d819e0d755eb7345be74
yoluwo <yetunde.oluwo@vogappdevelopers.com>
2022-07-26 12:25:01 -0600

    Day1 Assignment

---
+++ DayOne/datatypes.js
@@ -0,0 +1,44 @@
+//Question 5
+
+console.log(typeof first_Name)
+console.log(typeof checkStatus)
+console.log(typeof thing)
+console.log(typeof something)
+
+//Question 6
+let numbero;
+let cityName;
+let isAwake;
+let carModel;
+
+//Question 7
+let num = 1;
+let city = "Lagos";
+let isUp = false;
+let carColor = "blue";
+
+//Question 8
+console.log(num)
+console.log(city)
+console.log(isUp)
+console.log(carColor)
+
+//Question 9
+let firstName = "Yetunde";
+let lastName = "Simon";
+let maritalStatus = false;
+let country = "Canada";
+let age = 31;
+
+console.log(firstName);
+console.log(lastName);
+console.log(maritalStatus);
+console.log(age);
+
+console.log(firstName, lastName, maritalStatus, age);
+
+//Question 10
+let myAge = 31;
+let yourAge = 17;
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
