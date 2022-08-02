f3607cf39b7c79dc20b72fb785a5a9b722270a80
yoluwo <yetunde.oluwo@vogappdevelopers.com>
2022-08-02 14:44:45 -0600

    Exercises for Day 2

---
+++ DayTwo/day2class.js
@@ -0,0 +1,55 @@
+//Exercises
+
+//Exercise level 1: Question 1
+let challenge = '30 Days Of JavaScript'
+
+//Exercise level 1: Question 2
+console.log(challenge)
+
+//Exercise level 1: Question 3
+console.log(challenge.length)
+
+//Exercise level 1: Question 4
+console.log(challenge.toUpperCase())
+
+//Exercise level 1: Question 5
+console.log(challenge.toLowerCase())
+
+//Exercise level 1: Question 6
+console.log(challenge.substr(0,2))
+
+//Exercise level 1: Question 7
+console.log(challenge.substr(3,18))
+
+//Exercise level 1: Question 8
+console.log(challenge.includes("Script"))
+
+//Exercise level 1: Question 9
+console.log(challenge.split())
+
+//Exercise level 1: Question 10
+console.log(challenge.split(' '))
+
+//Exercise level 1: Question 11
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(companies.split(','))
+console.log(companies.split())
+
+//Exercise level 1: Question 12
+console.log(challenge.replace('JavaScript', 'Python'))
+
+//Exercise level 1: Question 13
+console.log(challenge.charAt(15))
+
+//Exercise level 1: Question 14
+let positionOfJ = challenge.indexOf('J')
+//console.log(positionOfJ)
+console.log(challenge.charCodeAt(11))
+
+//Exercise level 1: Question 15
+console.log(challenge.indexOf('a'))
+
+//Exercise level 1: Question 16
+console.log(challenge.lastIndexOf('a'))
+
+//Exercise level 1: Question 17
